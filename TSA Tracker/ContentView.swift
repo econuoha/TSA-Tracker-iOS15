cf6cf324f7c5eceb12cdcf81f7ab44968f3e9c9a
Emmanuel Onuoha <emanonuoha@gmail.com>
2022-06-26 22:21:26 -0500

    Initial Commit

---
+++ TSA Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TSA Tracker
+//
+//  Created by Emmanuel Onouha on 6/26/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TSA Tracker/TSA_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TSA_TrackerApp.swift
+//  TSA Tracker
+//
+//  Created by Emmanuel Onouha on 6/26/22.
+//
+
+import SwiftUI
+
+@main
+struct TSA_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
